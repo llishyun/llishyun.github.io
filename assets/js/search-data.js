bb33b78b3c0f05e728076fe5cbc098b1d671fe59
llishyun <66715457+llishyun@users.noreply.github.com>
2026-02-23 08:44:45 +0000

    Deploying to gh-pages from @ llishyun/llishyun.github.io@3d7cde3034222f2bf67caee20fd41911561d8a00 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -388,7 +388,7 @@ ninja.data = [{
           title: '✨ FreeAction is accepted to CoRL 2025 LSRW workshop',
           description: "",
           section: "News",},{id: "news-affostruction-is-accepted-to-cvpr-2026",
-          title: '✨ Affostruction is accepted to CVPR 2026.',
+          title: 'Affostruction is accepted to CVPR 2026 ✨',
           description: "",
           section: "News",},{id: "news-joined-postech-cvlab-as-a-graduate-student",
           title: 'Joined POSTECH CVLab as a graduate student 👩‍🎓',
